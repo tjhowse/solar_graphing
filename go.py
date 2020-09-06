707659ccc5df6fe2b3354027c25ad7d66ebba81d
tjhowse <tjhowse@gmail.com>
2020-09-06 21:16:07 +1000

    .

--- go.py
+++ go.py
@@ -2,43 +2,47 @@
 # https://www.influxdata.com/blog/getting-started-python-influxdb/
 
 from influxdb import InfluxDBClient
+from cycler import cycler
 import matplotlib.pyplot as plt
 import numpy as np
 
 client = InfluxDBClient(host="192.168.1.50", port=8086)
 client.switch_database('telegraf')
 
-def solar_data(client, days_ago):
+def solar_data(client, series, days_ago):
                 # SELECT "pv1_power"
                 # SELECT "pv2_power"
                 # SELECT "total_pv_power"
-    query = '''
-                SELECT "total_pv_power"
-                FROM "mqtt_consumer"
-                WHERE ("topic" = '6hull/solar') AND
-                    time > now() - {}d AND
-                    time < now() - {}d
-            '''
     # query = '''
     #             SELECT "total_pv_power"
     #             FROM "mqtt_consumer"
     #             WHERE ("topic" = '6hull/solar') AND
-    #                 time > 2020-08-24T05:00 + {}d AND
-    #                 time < 2020-08-24T19:00 + {}d
+    #                 time > now() - {}d AND
+    #                 time < now() - {}d
     #         '''
-    results = client.query(query.format(days_ago,days_ago-1))
+    query = '''
+                SELECT "{}"
+                FROM "mqtt_consumer"
+                WHERE ("topic" = '6hull/solar') AND
+                    time > '2020-08-23T06:00:00Z' - 10h + {}d AND
+                    time < '2020-08-23T18:00:00Z' - 10h + {}d
+            '''
+    results = client.query(query.format(series, days_ago, days_ago))
     points = list(results.get_points())
 
     # times = [x["time"] for x in points]
-    values = [x["total_pv_power"] for x in points]
+    values = [x[series] for x in points]
     return values
 # print(times)
 # print(values)
 
 # print x_val
-plt.plot(solar_data(client,1), label='Today', linewidth=1, c="purple")
-plt.plot(solar_data(client,2), label='Yesterday', linewidth=1, c="red")
-plt.plot(solar_data(client,3), label='Day before yesterday', linewidth=1, c="blue")
+plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y', 'm']) +
+                           cycler('linestyle', ['-', '-', '-', '-', '-'])))
+for i in range(5):
+    plt.plot(solar_data(client, "pv1_power", i), label=i, linewidth=1)
+# plt.plot(solar_data(client, "pv1_power", 1), label='Yesterday', linewidth=1)
+# plt.plot(solar_data(client, "pv1_power", 2), label='Day before yesterday', linewidth=1)
 # plt.plot(times, values)
 # plt.plot_date(times, values, xdate=True, ydate=False)
 plt.xlabel("Time")
